83c0773a1e48ce68f0fc3e19b008b0a2c4676d9b
BlackBird-911 <iftikharimrul@iut-dhaka.edu>
2022-04-18 03:47:19 +0600

    sqlsss

--- User/php files/trainList.php
+++ User/php files/trainList.php
@@ -12,6 +12,28 @@
         $arrival = $row['ARRIVAL_TIME'];
         $departure = $row['DEPARTURE_TIME'];
         
+        $sql = "SELECT COUNT(TICKET_ID) AS SOLD FROM TICKETS WHERE TRAIN_ID = $tid AND TRAVEL_DATE = 
+                    TO_DATE('$date', 'YYYY-MM-DD') AND CLASS = '$class'";
+        $stid = oci_parse($conn, $sql);
+        oci_execute($stid);
+        $row = oci_fetch_assoc($stid);
+        $seatSold = $row['SOLD'];
+
+        $sql2= "SELECT COUNT(BID) AS CBID FROM BLOCKS WHERE TID = $tid and CLASS = '$class' ";
+        $stid2 = oci_parse($conn, $sql2);
+        oci_execute($stid2);
+        $data = oci_fetch_assoc($stid2);
+        $totalBlocks = $data['CBID'];
+
+
+        $availableSeats = $totalBlocks*30 - $seatSold;
+        if($availableSeats > 0){
+            $sql3 = "SELECT SID, SEAT_NUM FROM SEATS WHERE SID NOT IN 
+                    (SELECT SID FROM TICKETS WHERE TRAIN_ID = $tid AND TRAVEL_DATE = 
+                    TO_DATE('$date', 'YYYY-MM-DD') AND CLASS = '$class')"
+        }
+        
+
     }
 ?>
 
