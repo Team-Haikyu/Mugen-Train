7081a7c1eb7811359fed80c3e7a083bdc83746d5
BlackBird-911 <iftikharimrul@iut-dhaka.edu>
2022-04-17 23:50:07 +0600

    cdi

--- auth/functions/functionsForUser.php
+++ auth/functions/functionsForUser.php
@@ -151,30 +151,5 @@ function loginAdmin($useremail, $password){
 }
 
 
-function searchTrain($source, $dest, $class){
-    $sql = "SELECT ROUTE_ID FROM ROUTES WHERE SOURCE = '$source' AND DEST = '$dest' ";
-    $stid = oci_parse($conn, $sql);
-    $r = oci_execute($stid);
-    if (!$r) {
-        $m = oci_error($stid); 
-        trigger_error('Could not execute statement: '. $m['message'] . "  ". $dob, E_USER_ERROR);
-                header("location:user_register.php?error=stmtfailed");
-        exit();
-    }
-
-    $row = oci_fetch_assoc($stid);
-    $rid = $row['ROUTE_ID'];
-
-    $sql2 = "SELECT * FROM ROUTE_TRAIN_JUNCTION WHERE ROUTE_ID = $rid ";
-    $stid2 = oci_parse($conn, $sql2);
-    $r = oci_execute($stid);
-    if (!$r) {
-        $m = oci_error($stid); 
-        trigger_error('Could not execute statement: '. $m['message'] . "  ". $dob, E_USER_ERROR);
-                header("location:user_register.php?error=stmtfailed");
-        exit();
-    }
-    return $stid2; 
-}
 
 ?>
