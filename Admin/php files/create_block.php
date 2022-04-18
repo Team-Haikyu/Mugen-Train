185b0dc3db51d54ffa9deacc2c4b7a24d81cf823
Parasite-231 <muktadir@iut-dhaka.edu>
2022-04-18 06:41:27 +0600

    icns

--- Admin/php files/create_block.php
+++ Admin/php files/create_block.php
@@ -100,25 +100,25 @@
 </li>
 <li class="nav-item ">
 <a class="nav-link" href="adminTrainList.php" style="color: white;">
-  <i class='bx bxs-user-circle' ></i>
+<i class='bx bxs-file'></i>
   Train Lists
 </a>
 </li>
 <li class="nav-item ">
 <a class="nav-link" href="adminSeatList.php" style="color: white;">
-  <i class='bx bxs-user-circle' ></i>
+<i class='bx bx-accessibility'></i>
   Seat Lists
 </a>
 </li>
 <li class="nav-item ">
 <a class="nav-link" href="adminRouteList.php" style="color: white;">
-  <i class='bx bxs-user-circle' ></i>
+<i class='bx bxs-caret-up-circle'></i>
   Route Lists
 </a>
 </li>
 <li class="nav-item ">
 <a class="nav-link" href="adminBlockList.php" style="color: white;">
-  <i class='bx bxs-user-circle' ></i>
+<i class='bx bx-collection'></i>
   Block List
 </a>
 </li>
